e903107720ecd1e426cc432eeff002c895cb3b78
Steffen Neumann <sneumann@ipb-halle.de>
2010-12-15 17:09:39 +0100

    fixed test to select only one file for rampOpen

--- inst/unitTests/runit.ramp.R
+++ inst/unitTests/runit.ramp.R
@@ -7,7 +7,7 @@ test.filenotfound <- function() {
 test.mzXML <- function() {
     library(msdata)
     cdfpath <- system.file("threonine", package = "msdata")
-    file <- list.files(cdfpath, pattern=".mzXML",
+    file <- list.files(cdfpath, pattern="threonine_i2_e35_pH_tree.mzXML",
                        full.names=TRUE, recursive = TRUE)
     ramp <- rampOpenFile(file)
     ramp$getRunInfo()
@@ -17,7 +17,7 @@ test.mzXML <- function() {
 test.mzML <- function() {
     library(msdata)
     cdfpath <- system.file("microtofq", package = "msdata")
-    file <- list.files(cdfpath, pattern=".mzML",
+    file <- list.files(cdfpath, pattern="MM14.mzML",
                        full.names=TRUE, recursive = TRUE)
     ramp <- rampOpenFile(file)
     ramp$getRunInfo()
@@ -27,7 +27,7 @@ test.mzML <- function() {
 test.mzData <- function() {
     library(msdata)
     cdfpath <- system.file("microtofq", package = "msdata")
-    file <- list.files(cdfpath, pattern=".mzdata",
+    file <- list.files(cdfpath, pattern="MM14.mzdata",
                        full.names=TRUE, recursive = TRUE)
     ramp <- rampOpenFile(file)
 
